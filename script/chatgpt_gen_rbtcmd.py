8ed08c69d975138b3a2adfaa692c438c0794adee
wangyan-hlab <wangyan94maomao@gmail.com>
2023-05-04 15:49:37 +0800

    minor

--- script/chatgpt_gen_rbtcmd.py
+++ script/chatgpt_gen_rbtcmd.py
@@ -1,23 +1,27 @@
 # Instead of the terminal mode, you can also try ChatGPT in a python script
 # Here is an example.
-#%% Importing libs
+#%% 
+# Importing libs
 import os
 from frchat.frchatbot import FRChatBot
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv("dev.env"))  # read local .env file
 
-#%% Providing some context to start a conversation
+#%% 
+# Providing a context to start a conversation
 messages = [
     {'role':'system', 'content':'你是一个机器人用户助手,帮助用户控制一个6关节机器人运动,你需要组合使用一些机器人控制函数来完成特定任务,只使用用户提供的函数,不要自行引入其他第三方库(如RoboDK)'},
     {'role':'user', 'content':'你好，我们开始编写机器人控制指令吧'},
     {'role':'assistant', 'content':'好的，请告诉我能够使用哪些机器人控制函数'},
 ]
 
-#%% Defining a ChatBot and the CONFIG_DIR
+#%% 
+# Defining a ChatBot and claiming the CONFIG_DIR
 frcb = FRChatBot(messages, temperature=0.1)
 config_dir = os.path.join(os.path.dirname(os.path.split(__file__)[0]), "config/fr_robot/")
 
-#%%
+#%% 
+# Providing some functions
 file = config_dir+'robot_movement/jog.yaml'
 output = frcb.generate_function_list(file)
 prompt = f"""
@@ -26,14 +30,16 @@ prompt = f"""
 """
 frcb.chat(prompt)
 
-#%%
+#%% 
+# Describing a task
 prompt = f"""
     任务1:请开始点动,1关节旋转,单次最大旋转5度,旋转到正30度,可使用循环
     请生成该任务的python指令
 """
 frcb.chat(prompt)
 
-#%% New functions
+#%% 
+# Providing more functions
 file = config_dir+'robot_statusinquiry.yaml'
 output = frcb.generate_function_list(file)
 prompt = f"""
@@ -42,4 +48,12 @@ prompt = f"""
     请重写之前的指令，可使用关节角度获取函数获取关节位置
 """
 frcb.chat(prompt)
+#%% 
+# Expanding the task
+prompt = f"""
+    任务2:关节配置1-[0,0,0,0,0,0],关节配置2-[30,-60,90,20,-10,30], \
+    关节点动从配置1运动到配置2,请帮我生成python指令,注意不要使用用户未提供的函数
+"""
+frcb.chat(prompt)
+
 # %%
--- script/frchat/frchatbot.py
+++ script/frchat/frchatbot.py
@@ -1,27 +1,29 @@
-# 
 import openai
 import yaml
 
 class FRChatBot(object):
+    """
+        A ChatBot generating command to control FR Cobots
+
+        Author: wangyan
+        Date: 2023/05/04
+    """
     
     def __init__(self, 
                  messages, 
                  temperature=0.3, 
                  model="gpt-3.5-turbo") -> None:
-        
         self.messages = messages
-        self.temperature = temperature
+        self.temperature = temperature  # this is the degree of the randomness of the response
         self.model = model
 
     def get_completion_from_messages(self, messages, temperature, model):
-
         response = openai.ChatCompletion.create(
             model=model,
             messages=messages,
-            temperature=temperature,    # this is the degree of the randomness of the response
+            temperature=temperature,
         )
         # print(str(response.choices[0].message))
-
         return response.choices[0].message["content"]
 
     def build_message(self, role, content):
@@ -29,7 +31,6 @@ class FRChatBot(object):
         return message
 
     def chat(self, prompt):
-
         print("USER:", prompt)
         self.messages.append(self.build_message('user', prompt))
         response = self.get_completion_from_messages(messages=self.messages, 
@@ -39,10 +40,8 @@ class FRChatBot(object):
         self.messages.append(self.build_message('assistant', response))
 
     def generate_function_list(self, file):
-
         with open(file, "rb") as f:
             functions = yaml.safe_load(f)
-
         def read_function(func):
             func_name = func['name']
             args = ", ".join(arg['name'] for arg in func['arguments'])
@@ -50,7 +49,5 @@ class FRChatBot(object):
             arg_desc = ", ".join(f"{arg['name']}:{arg['description']}({arg['type']}型变量)" for arg in func['arguments'])
             ret = func['return']
             return [f"{func_name}", f"功能:'{desc}'", f"参数:'{arg_desc}'", f"返回值:'{ret}'"]
-    
         output = [read_function(func['function']) for func in functions]
-
         return output
