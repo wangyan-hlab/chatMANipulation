a8e558843f041db259fe35bbdeb5ca5a33b58715
wangyan-hlab <wangyan94maomao@gmail.com>
2023-05-17 11:24:51 +0800

    move the module

--- script/chatbot_gui.py
+++ script/chatbot_gui.py
@@ -5,7 +5,6 @@
 # date: 2023/05/09
 
 # Importing libs
-from frchat.frchatbot import FRChatBot
 import re
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -15,14 +14,22 @@ import tkinter as tk
 from tkinter import filedialog
 import datetime
 
+import sys
+sys.path.append("../")
+from frchat.frchatbot import FRChatBot
+
 # 设置ChatBot
 ## Providing a context to start a conversation topic
 messages = [
     {'role':'system', 'content':'你是一个机器人用户助手,帮助用户控制一个6关节机器人运动, \
      你需要组合使用一些函数来完成特定任务,只使用用户提供的函数,不要自行引入其他第三方库(如RoboDK)'},
     {'role':'user', 'content':'你好,请编写机器人控制指令,假设已使用如下代码实例化对象： \
-     ```import frrpc\n \
-     robot=frrpc.RPC("192.168.58.2")```, \
+     ``` \
+     import sys\n \
+     sys.path.append("../")\n \
+     from fr_python_sdk.frrpc import RPC\n \
+     robot = RPC("192.168.58.2") \
+     ```, \
      之后用户提供的函数均为对象的方法.请在输出程序时加上机器人实例化部分的指令'},
     {'role':'assistant', 'content':'好的，请告诉我能够使用哪些函数'},
     {'role':'user','content':'我需要添加一些约束条件：最终生成的代码都需要是可执行的函数，并返回函数名称'},
