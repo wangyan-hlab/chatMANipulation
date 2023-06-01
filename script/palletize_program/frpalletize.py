9d2d9f25ca09f4199a8f90eb53b34c70f782b001
wangyan-hlab <wangyan94maomao@gmail.com>
2023-06-01 17:43:44 +0800

    fit more situations

--- script/palletize_program/frpalletize.py
+++ script/palletize_program/frpalletize.py
@@ -4,6 +4,7 @@ import frmovewrapper.robotmath as rm
 import copy
 import time
 
+
 class FRPalletize(object):
     """
         A Palletization template
@@ -18,16 +19,28 @@ class FRPalletize(object):
         # 读取参数配置
         self.params = params
 
+
     def euler_to_homomat(self, euler):
+        for i in range(3,6):
+            euler[i] = np.deg2rad(euler[i])
         rotmat = rm.rotmat_from_euler(euler[3], euler[4], euler[5])
         return rm.homomat_from_posrot(np.asarray(euler[:3]), rotmat)
 
+
     def euler_from_homomat(self, homomat):
         rpy = rm.rotmat_to_euler(homomat[:3, :3])
-        return np.concatenate((homomat[:3, 3], rpy))
+        euler = np.concatenate((homomat[:3, 3], rpy))
+        for i in range(3,6):
+            euler[i] = np.rad2deg(euler[i])
+        for i in range(6):
+            euler = list(euler)
+            euler[i] = float(euler[i])
+        return euler
+        
 
     def get_target_point(self, box_point, first_corner, move_direction, 
-                         p_path, p_pallet_origin, p_suction_point, appr_offset=[50,50,50]):
+                         p_path, p_pathz, p_pallet_origin, p_suction_point, 
+                         appr_offset=[50,50,50]):
         """
             获得机器人坐标系下的机器人运动目标点
 
@@ -41,84 +54,61 @@ class FRPalletize(object):
         """
         (x, y, z) = box_point
 
-        for i in range(3,6):
-            p_pallet_origin[i] = np.deg2rad(p_pallet_origin[i])
-            p_suction_point[i] = np.deg2rad(p_suction_point[i])
-
         tran_tcp_ppo_to_robot = self.euler_to_homomat(p_pallet_origin)  # 原点和第一个箱子几何中心重合,ppo=p_pallet_origin
         tran_tcp_psp_to_robot = self.euler_to_homomat(p_suction_point) # psp=p_suction_point
         tran_tcp_path_to_robot = self.euler_to_homomat(p_path)
-
-        tran_path_to_ppo = np.linalg.inv(tran_tcp_ppo_to_robot) @ tran_tcp_path_to_robot
+        tran_tcp_pathz_to_robot = self.euler_to_homomat(p_pathz)
         
-        theta = np.arctan2(tran_path_to_ppo[1, 3], tran_path_to_ppo[0, 3])
-
+        z_pallet_to_robot = tran_tcp_pathz_to_robot[:3,3] - tran_tcp_path_to_robot[:3,3]
         if first_corner == [0,0]:
             if move_direction == 'X':
-                tran_pallet_to_tcp = np.array([
-                    [np.cos(theta), np.sin(theta), 0, 0],
-                    [np.sin(theta), -np.cos(theta), 0, 0],
-                    [0, 0, -1, 0],
-                    [0, 0, 0, 1]])
+                x_pallet_to_robot = tran_tcp_path_to_robot[:3,3] - tran_tcp_ppo_to_robot[:3,3]
+                y_pallet_to_robot = np.cross(z_pallet_to_robot, x_pallet_to_robot)
             elif move_direction == 'Y':
-                tran_pallet_to_tcp = np.array([
-                    [-np.sin(theta), np.cos(theta), 0, 0],
-                    [np.cos(theta), np.sin(theta), 0, 0],
-                    [0, 0, -1, 0],
-                    [0, 0, 0, 1]])
+                y_pallet_to_robot = tran_tcp_path_to_robot[:3,3] - tran_tcp_ppo_to_robot[:3,3]
+                x_pallet_to_robot = np.cross(y_pallet_to_robot, z_pallet_to_robot)
             
         elif first_corner == [0,1]:
             if move_direction == 'X':
-                tran_pallet_to_tcp = np.array([
-                    [np.cos(theta), np.sin(theta), 0, 0],
-                    [np.sin(theta), -np.cos(theta), 0, 0],
-                    [0, 0, -1, 0],
-                    [0, 0, 0, 1]])
+                x_pallet_to_robot = tran_tcp_path_to_robot[:3,3] - tran_tcp_ppo_to_robot[:3,3]
+                y_pallet_to_robot = np.cross(z_pallet_to_robot, x_pallet_to_robot)
             elif move_direction == 'Y':
-                tran_pallet_to_tcp = np.array([
-                    [np.sin(theta), -np.cos(theta), 0, 0],
-                    [-np.cos(theta), -np.sin(theta), 0, 0],
-                    [0, 0, -1, 0],
-                    [0, 0, 0, 1]])
+                y_pallet_to_robot = -(tran_tcp_path_to_robot[:3,3] - tran_tcp_ppo_to_robot[:3,3])
+                x_pallet_to_robot = np.cross(y_pallet_to_robot, z_pallet_to_robot)
 
         elif first_corner == [1,0]:
             if move_direction == 'X':
-                tran_pallet_to_tcp = np.array([
-                    [-np.cos(theta), -np.sin(theta), 0, 0],
-                    [-np.sin(theta), np.cos(theta), 0, 0],
-                    [0, 0, -1, 0],
-                    [0, 0, 0, 1]])
+                x_pallet_to_robot = -(tran_tcp_path_to_robot[:3,3] - tran_tcp_ppo_to_robot[:3,3])
+                y_pallet_to_robot = np.cross(z_pallet_to_robot, x_pallet_to_robot)
             elif move_direction == 'Y':
-                tran_pallet_to_tcp = np.array([
-                    [-np.sin(theta), np.cos(theta), 0, 0],
-                    [np.cos(theta), np.sin(theta), 0, 0],
-                    [0, 0, -1, 0],
-                    [0, 0, 0, 1]])
+                y_pallet_to_robot = tran_tcp_path_to_robot[:3,3] - tran_tcp_ppo_to_robot[:3,3]
+                x_pallet_to_robot = np.cross(y_pallet_to_robot, z_pallet_to_robot)
                 
         elif first_corner == [1,1]:
             if move_direction == 'X':
-                tran_pallet_to_tcp = np.array([
-                    [-np.cos(theta), -np.sin(theta), 0, 0],
-                    [-np.sin(theta), np.cos(theta), 0, 0],
-                    [0, 0, -1, 0],
-                    [0, 0, 0, 1]])
+                x_pallet_to_robot = -(tran_tcp_path_to_robot[:3,3] - tran_tcp_ppo_to_robot[:3,3])
+                y_pallet_to_robot = np.cross(z_pallet_to_robot, x_pallet_to_robot)
             elif move_direction == 'Y':
-                tran_pallet_to_tcp = np.array([
-                    [np.sin(theta), -np.cos(theta), 0, 0],
-                    [-np.cos(theta), -np.sin(theta), 0, 0],
-                    [0, 0, -1, 0],
-                    [0, 0, 0, 1]])
+                y_pallet_to_robot = -(tran_tcp_path_to_robot[:3,3] - tran_tcp_ppo_to_robot[:3,3])
+                x_pallet_to_robot = np.cross(y_pallet_to_robot, z_pallet_to_robot)
         
-        tran_pallet_to_robot = tran_tcp_ppo_to_robot @ tran_pallet_to_tcp
+        tran_pallet_to_robot_rotmat = np.vstack([rm.unit_vector(x_pallet_to_robot), 
+                                                rm.unit_vector(y_pallet_to_robot), 
+                                                rm.unit_vector(z_pallet_to_robot)]).T
+        tran_pallet_to_robot = rm.homomat_from_posrot(pos=tran_tcp_ppo_to_robot[:3,3], 
+                                                      rot=tran_pallet_to_robot_rotmat)
+        tran_pallet_to_tcp = rm.homomat_from_posrot(pos=np.zeros(3),
+                                                    rot=np.linalg.inv(tran_tcp_ppo_to_robot[:3,:3]) @ tran_pallet_to_robot_rotmat)
         tran_suction_to_tcp = tran_pallet_to_tcp
-        
         tran_suction_to_robot = tran_tcp_psp_to_robot @ tran_suction_to_tcp
-        tran_suction_to_box = np.linalg.inv(tran_pallet_to_robot) @ tran_suction_to_robot
+        tran_suction_to_box = rm.homomat_from_posrot(
+            pos=np.linalg.inv(tran_pallet_to_robot[:3,:3]) @ (tran_suction_to_robot[:3,3]-tran_pallet_to_robot[:3,3]),
+            rot=np.linalg.inv(tran_pallet_to_robot[:3,:3]) @ tran_suction_to_robot[:3,:3])
 
         # 目标点计算
         tran_box_to_pallet = self.euler_to_homomat([x, y, z, 0., 0., 0.])
         tran_suctionbox_to_robot = tran_pallet_to_robot @ (tran_box_to_pallet @ tran_suction_to_box)
-        tran_tcp_to_robot_target = tran_suctionbox_to_robot @ np.linalg.inv(tran_suction_to_tcp)
+        tran_tcp_to_robot_target = tran_suctionbox_to_robot @ rm.homomat_inverse(tran_suction_to_tcp)
         target_point = self.euler_from_homomat(tran_tcp_to_robot_target)
         # 目标渐近点计算
         if first_corner == [0, 0]:
@@ -130,17 +120,8 @@ class FRPalletize(object):
         elif first_corner == [1, 1]:
             tran_boxappr_to_pallet = self.euler_to_homomat([x-appr_offset[0], y-appr_offset[1], z+appr_offset[2], 0., 0., 0.])
         tran_suctionappr_to_robot = tran_pallet_to_robot @ (tran_boxappr_to_pallet @ tran_suction_to_box)
-        tran_tcp_to_robot_targetappr = tran_suctionappr_to_robot @ np.linalg.inv(tran_suction_to_tcp)
+        tran_tcp_to_robot_targetappr = tran_suctionappr_to_robot @ rm.homomat_inverse(tran_suction_to_tcp)
         target_appr_point = self.euler_from_homomat(tran_tcp_to_robot_targetappr)
-
-        for i in range(3,6):
-            target_point[i] = np.rad2deg(target_point[i])
-            target_appr_point[i] = np.rad2deg(target_appr_point[i])
-        for i in range(6):
-            target_point = list(target_point)
-            target_point[i] = float(target_point[i])
-            target_appr_point = list(target_appr_point)
-            target_appr_point[i] = float(target_appr_point[i])
 
         return target_point, target_appr_point
 
@@ -170,7 +151,6 @@ class FRPalletize(object):
         # 托盘参数
         pallet_length = self.params['托盘配置']['前边长度']
         pallet_width = self.params['托盘配置']['侧边长度']
-        pallet_height = self.params['托盘配置']['托盘高度']
 
         # 模式参数
         box_interval = self.params['模式配置']['工件间隔']
@@ -183,7 +163,8 @@ class FRPalletize(object):
         p_prepare = self.params['机器人移动配置']['作业准备点']
         p_trans = self.params['机器人移动配置']['工位过渡点']
         p_pallet_origin = self.params['机器人移动配置']['托盘原点']
-        p_path = self.params['机器人移动配置']['路径点']
+        p_path = self.params['机器人移动配置']['平面路径点']
+        p_pathz = self.params['机器人移动配置']['法向路径点']
         first_corner = self.params['机器人移动配置']['起始方位']
         move_direction = self.params['机器人移动配置']['移动方向']
         motion = self.params['机器人移动配置']['运动方式']
@@ -363,12 +344,14 @@ class FRPalletize(object):
                 suction_point = copy.deepcopy(p_suction_point)
                 trans_point = copy.deepcopy(p_trans)
                 path_point = copy.deepcopy(p_path)
+                path_pointz = copy.deepcopy(p_pathz)
                 p_trans_zoffset = index//(nrow*ncol)*box_height
                 trans_point[2] += p_trans_zoffset
                 target_point, target_appr_point = self.get_target_point(box_point, 
                                                     first_corner, 
                                                     move_direction,
                                                     p_path=path_point,
+                                                    p_pathz=path_pointz,
                                                     p_pallet_origin=pallet_origin, 
                                                     p_suction_point=suction_point) 
                 print(f"托盘坐标系下第{index+1}个工件位置: {box_point}")
@@ -410,6 +393,7 @@ class FRPalletize(object):
                 suction_point = copy.deepcopy(p_suction_point)
                 trans_point = copy.deepcopy(p_trans)
                 path_point = copy.deepcopy(p_path)
+                path_pointz = copy.deepcopy(p_pathz)
                 home_point = copy.deepcopy(p_home)
                 p_trans_zoffset = (nlayer - 1 - index//(nrow*ncol))*box_height
                 trans_point[2] += p_trans_zoffset
@@ -417,6 +401,7 @@ class FRPalletize(object):
                                                     first_corner, 
                                                     move_direction,
                                                     p_path=path_point,
+                                                    p_pathz=path_pointz,
                                                     p_pallet_origin=pallet_origin, 
                                                     p_suction_point=suction_point) 
                 print(f"托盘坐标系下第{index+1}个工件位置: {box_point}")
@@ -455,16 +440,3 @@ class FRPalletize(object):
                 
         else:
             raise ValueError("无效的堆叠方式,必须是'load'或'unload'")
-
-# from frmovewrapper import robotmath as rm
-# import numpy as np
-
-# xa = np.array([1, 0, 0])
-# ya = np.array([0, 1, 0])
-# za = np.array([0, 0, 1])
-
-# x_new = np.array([1, 1, 1])
-# z_new = np.array([1, 1, -2])
-# y_new = np.cross(x_new, z_new)
-# print(y_new)
-# print(rm.unit_vector(x_new), rm.unit_vector(y_new), rm.unit_vector(z_new))
